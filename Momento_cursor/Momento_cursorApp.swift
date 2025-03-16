390664e2e48e79cff5dca72d114b35de9db99fd6
Yaehoon Kim <eraser3031@gmail.com>
2025-03-16 13:31:30 +0900

    Initial Commit

---
+++ Momento_cursor/Momento_cursorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Momento_cursorApp.swift
+//  Momento_cursor
+//
+//  Created by 김예훈 on 3/16/25.
+//
+
+import SwiftUI
+
+@main
+struct Momento_cursorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
